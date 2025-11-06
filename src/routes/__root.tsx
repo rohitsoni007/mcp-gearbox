573dab1aa4bc7a8f6dd7b81afaa1f1a31fcf3a00
rohit <rohitsoni259@gmail.com>
2025-11-06 12:19:19 +0530

    redesign

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,14 +1,11 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 // import { TanStackRouterDevtools } from '@tanstack/router-devtools';
-import Layout from '../components/Layout';
-import NotFound from '../pages/NotFound';
 
 export const Route = createRootRoute({
   component: () => (
-    <Layout>
+    <>
       <Outlet />
       {/* <TanStackRouterDevtools /> */}
-    </Layout>
-  ),
-  errorComponent: NotFound,
+    </>
+  )
 });
